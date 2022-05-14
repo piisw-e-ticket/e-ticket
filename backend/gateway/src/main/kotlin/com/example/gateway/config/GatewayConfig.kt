ba0b0368e13bbeac6a12ebde5339716cf083bd7a
Rafal Kiszczyszyn <rafalkiszczyszyn09@gmail.com>
2022-05-14 16:59:45 +0200

    Handle both auth cookies and auth header in JwtFilter

--- backend/gateway/src/main/kotlin/com/example/gateway/config/GatewayConfig.kt
+++ backend/gateway/src/main/kotlin/com/example/gateway/config/GatewayConfig.kt
@@ -1,5 +1,6 @@
 package com.example.gateway.config
 
+import com.example.gateway.filters.JwtFilter
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
@@ -18,11 +19,9 @@ class GatewayConfig {
 
     @Bean
     fun routes(builder: RouteLocatorBuilder): RouteLocator? {
-        return builder.routes().route("auth") { r: PredicateSpec -> r.path("/auth/**").filters { f: GatewayFilterSpec -> f.filter(filter) }.uri("lb://auth") }
-//                .route("alert") { r: PredicateSpec -> r.path("/alert/**").filters { f: GatewayFilterSpec -> f.filter(filter) }.uri("lb://alert") }
-//                .route("echo") { r: PredicateSpec -> r.path("/echo/**").filters { f: GatewayFilterSpec -> f.filter(filter) }.uri("lb://echo") }
-//                .route("hello") { r: PredicateSpec -> r.path("/hello/**").filters { f: GatewayFilterSpec -> f.filter(filter) }.uri("lb://hello") }
-                .build()
+        return builder.routes().route("auth") { p: PredicateSpec ->
+            p.path("/auth/**").filters { f: GatewayFilterSpec -> f.filter(filter) }.uri("lb://auth")
+        }.build()
     }
 
 }
