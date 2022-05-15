d58f82795371d9c6d9f2dc32cb617960ad35344f
kwojcikowski <wojcikowskikacper5@gmail.com>
2022-05-15 22:45:46 +0200

    Added register fields validation

--- backend/auth/src/main/kotlin/com/example/auth/dto/AuthDtos.kt
+++ backend/auth/src/main/kotlin/com/example/auth/dto/AuthDtos.kt
@@ -2,10 +2,23 @@ package com.example.auth.dto
 
 import com.example.auth.model.Role
 import java.util.*
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Pattern
 
 // Requests
 data class LoginDto(val username: String, val password: String)
-data class RegisterDto(val username: String, val password: String, val email: String)
+data class RegisterDto(
+
+        @field:Pattern(regexp = "[a-z0-9]{4,15}", message = "Should contain only lowercase letters or numbers and be 4-15 characters long.")
+        val username: String,
+
+        @field:Pattern(regexp = "[a-zA-Z0-9]{4,}", message = "Should contain only letters or numbers and be minimum 4 characters long.")
+        val password: String,
+
+        @field:NotBlank
+        @field:Pattern(regexp = "[a-z0-9.]+@[a-z0-9][a-z0-9.]*\\.[a-z]{2,3}", message = "The email address is incorrect.")
+        val email: String
+)
 
 // Responses
 data class JwtTokenPairDto(
